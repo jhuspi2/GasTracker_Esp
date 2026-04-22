907fa0c6fa243437a10e161e7b906eca21ec04c1
Jesús Piazuelo Roca <jhuspi2@gmail.com>
2026-04-22 15:12:23 +0200

    feat: add Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
 import App from './App.tsx';
 import './index.css';
 
@@ -15,5 +16,6 @@ if ('serviceWorker' in navigator) {
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 );
